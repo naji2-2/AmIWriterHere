5aa461a9183684202c32d74578bb69d3101c1e18
Najisu <s2322@e-mirim.hs.kr>
2024-11-16 16:44:26 +0900

    feat: 음악 재생 기능, 음악 파일 추가

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import pygame
 import tkinter as tk
 from ReadingScreen import ReadingScreen
 from StartScreen import StartScreen
@@ -13,6 +14,13 @@ class MainApp(tk.Tk):
         self.title("이곳에선 내가 작가?!")
         self.geometry("2280x1080")
         self.attributes("-fullscreen", True)
+
+        # 배경 음악 및 효과음 초기화
+        pygame.mixer.init()         # pygame 오디오 초기화
+        self.background_music = "music/background_music.mp3"    # 배경 음악 경로
+        self.button_click_sound = "music/button_click_sound.mp3"    # 버튼 클릭 효과음 경로
+        self.back_button_click_sound = "music/back_button_click_sound.mp3"  # 버튼 클릭 효과음 경로
+        self.play_background_music()
 
         # 등장인물, 키워드, 사건
         self.character = []
@@ -42,6 +50,22 @@ class MainApp(tk.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame("StartScreen")  # 첫 화면을 StartScreen으로 설정
+
+    # 배경음악 재생
+    def play_background_music(self):
+        pygame.mixer.music.load(self.background_music)
+        pygame.mixer.music.play(loops=-1)  # 무한 반복
+
+    # 버튼 클릭 효과음
+    def play_button_click_sound(self):
+        click_sound = pygame.mixer.Sound(self.button_click_sound)
+        #click_sound.set_volume(0.5)
+        click_sound.play()
+
+    # 돌아가기 버튼 클릭 효과음
+    def play_back_button_click_sound(self):
+        click_sound = pygame.mixer.Sound(self.back_button_click_sound)
+        click_sound.play()
 
     def new_random(self):
         return Random.random_character(), Random.random_keyword(), Random.random_incident()
